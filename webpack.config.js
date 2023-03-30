8ee5a71b9eec806a49ae67b62cdc849c19e128f1
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-03-30 01:42:20 -0700

    add linters

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+  mode: 'production',
   entry: './src/index.js',
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-        title: 'Output Management',
+      title: 'Output Management',
       template: './src/index.html',
     }),
   ],
   output: {
-    filename: "build/[name].js",
+    filename: 'build/[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
